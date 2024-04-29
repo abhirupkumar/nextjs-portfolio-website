dbdf0ad4a9bf4fb3ee5a77083dfefb0001bdaf69
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-04-29 16:16:23 +0530

    miscelleneous changes

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 const Logo = () => {
   return (
     <Link href="/">
-      {/* <span className='p-2 kalam-regular text-3xl text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-blue-500 rounded-md'>AKB</span> */}
       <span className='p-2 kalam-regular text-3xl logo-text'>AKB</span>
     </Link>
   );
