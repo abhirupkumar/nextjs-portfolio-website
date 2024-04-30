fc6ed17b14b771af0f2b54599f283a6dc3ea2e2f
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-04-30 14:36:33 +0530

    seo updates

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,8 +11,23 @@ import StarsCanvas from '@/components/StarBackground';
 const outfit = Outfit({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Abhirup Kumar Bhowmick',
+  title: 'Abhirup | Web Developer | App Developer | C++ Programmer',
   description: 'Portfolio Website',
+  image: "/hero/developer.png",
+  keywords: ['abhirup', 'kumar', 'bhowmick', 'abhirupkumar', 'portfolio', 'web development', 'app development', 'software geek', 'c++ programming'],
+  openGraph: {
+    title: 'Abhirup | Web Developer | App Developer | C++ Programmer',
+    description: 'Portfolio Website',
+    images: [
+      {
+        url: "/hero/developer.png"
+      }
+    ]
+  },
+  metadataBase: new URL('https://abhirupkumar.vercel.app'),
+  other: {
+    "google-site-verification": "co0B-CYrqRdI_fmi2ySiu2nYjJd2Wq8Db9ESsAv_uZE"
+  }
 };
 
 export default function RootLayout({ children }) {
